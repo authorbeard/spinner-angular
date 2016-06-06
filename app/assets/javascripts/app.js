35e29f431b78a70429ab4955396a9aae003b5bee
authorbeard <msnolan@gmail.com>
2016-06-06 19:08:08 -0400

    initial angular setup

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,17 @@
+angular 
+    .module('app', ['ui.router', 'templates'])
+    .config(function ($stateProvider, $urlRouterProvider){
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/views/home.html',
+                controller: 'HomeCtrl as home'
+
+
+            })
+
+
+        $urlRouterProvider.otherwise('/')
+
+
+    })
---
+++ app/assets/javascripts/app/controllers/homeCtrl.js
@@ -0,0 +1,9 @@
+function HomeCtrl(){
+  this.message = "home controller, bitches"
+
+
+}
+
+angular 
+    .module('app')
+    .controller('HomeCtrl', HomeCtrl)
